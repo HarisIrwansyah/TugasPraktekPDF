d8f50caa26105d0df260b28741b69b1c2594ecb2
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2017-11-19 13:14:06 +0800

    Mengubah Letak int main dan Menambah rumus
    nilai yang di konfersikan yaitu pembilang dan penyebut, kemudian diberi
    rumus untuk menentukan yang mana pembilang dan penyebut.

--- CthPraktek04.cpp
+++ CthPraktek04.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main (){
-	
+bool ConvertToLowestTerms(int &pembilang, int &penyebut);
+bool ConvertToLowestTerms(int &pembilang, int &penyebut)
+{
+if (penyebut == 0)
+return false;
+
+int FPB = penyebut;
+if (FPB > pembilang)
+FPB = pembilang;
+while (((pembilang % FPB) != 0) ||
+((penyebut % FPB) != 0))
+{
+FPB--;
+}
+pembilang /= FPB;
+penyebut /= FPB;
+return true;
+}
+
+int main( ){
+
 }
