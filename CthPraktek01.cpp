76cb155ba09328bd17f276328349227c2a9393a2
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2017-11-19 10:23:27 +0800

    PERBAIKAN, memasukkan variabelnya

--- CthPraktek01.cpp
+++ CthPraktek01.cpp
@@ -5,6 +5,7 @@ void input(int& jam24, int& menit);
 void konfersi(int& jam, char& AMPM);
 void output(int jam, int menit, char AMPM);
 
-int main (){
-	
+int main(){
+int jam, menit;
+char AMPM, ans;
 }
