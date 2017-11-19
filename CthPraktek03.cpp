2cff8a274dffc24bc7e971d6e38bed866ec45514
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2017-11-19 12:38:36 +0800

    PERBAIKAN, Menulis fungsi  "ConvertToMPH"
    mengetahui kecepatan menjalankan di Miles Per Jam. mengkonfersi nilai
    kecepatan perjam, menentukan hasilnya di output.

--- CthPraktek03.cpp
+++ CthPraktek03.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 using namespace std;
 
-int main (){
-	
+double ConvertToMPH(int kecepatanMin, int kecepatanPersec);
+double ConvertToMPH(double kph);
+double ConvertToMPH(int kecepatanMin, int kecepatanPersec){
+
+int secspermile;
+double mph;
+secspermile = kecepatanMin * 60 + kecepatanPersec;
+mph = (1 / static_cast<double>(secspermile)) * 3600;
+return mph;
+}
+
+double ConvertToMPH(double kph){
+double mph;
+mph = kph / 1.61;
+return mph;
+}
+
+int main(){
+cout << "Kecepatan 05:30 adalah " << ConvertToMPH(5,30) << " MPH." << endl;
 }
