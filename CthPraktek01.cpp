b1c0a3cc3a18f4f529194e87e7b0707c9751c715
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2017-11-19 10:31:21 +0800

    menentukan input, konfersi, dan outputnya
    menambahkan bagian pilihan jika ingin mengulang programnya. menyatakan
    fungsi pada konfersi nilai jika lebih dari jam 12, sama dengan 12, dan
    menentukan nilai jika kurang dari jam 12 dengan output AM / PM

--- CthPraktek01.cpp
+++ CthPraktek01.cpp
@@ -8,4 +8,40 @@ void output(int jam, int menit, char AMPM);
 int main(){
 int jam, menit;
 char AMPM, ans;
+do
+{
+input(jam, menit);
+konfersi(jam, AMPM);
+output(jam, menit, AMPM);
+cout << "Masukkan huruf Y atau y untuk mengulang, T atau t keluar : "<< endl; cin >> ans;
+} while('Y'== ans || 'y' == ans);
+return 0;
+}
+void input(int& jam24, int& menit)
+{
+char colon;
+system("cls");
+cout << "Masukkan 24 jam dengan format HH:MM"<< endl; cin >> jam24 >> colon >> menit;
+}
+void konfersi (int& jam, char& AMPM)
+{
+if(jam > 12) 
+{
+jam = jam - 12;
+AMPM = 'P';
+}
+else if (12 == jam)
+AMPM = 'P';
+else if (0 == jam)
+{
+jam = jam + 12;
+AMPM = 'A';
+}
+else
+AMPM = 'A';
+}
+void output(int jam, int menit, char AMPM)
+{
+system ("cls");
+cout << "Waktu dalam format 12 jam: " << endl << jam<< ":" << menit << " " << AMPM << 'M' << endl;
 }
